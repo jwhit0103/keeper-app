6d9853c1e41a79d05f1cba8e9538d24bfe0850ab
whitakerje <whitakerje@appstate.edu>
2023-04-10 21:57:39 -0400

    keeper-app part 2 looping over notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
+// eslint-disable-next-line
+
 import React from "react";
-import ReactDOM from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
-  return [<Header />, <Note />, <Footer />];
+  return (
+    <div>
+      <Header />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
 }
 export default App;
